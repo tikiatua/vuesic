6dcfe0223873c1fbf95ee0e88d65b82897027e1e
Max Gfeller <mg@cyon.ch>
2019-01-10 19:43:39 +0100

    Add YoutubeService

---
+++ src/lib/YoutubeService.js
@@ -0,0 +1,17 @@
+import { findBestMusicVideo } from 'youtube-best-video'
+
+let apiKey = process.env.VUE_APP_YOUTUBE_API_KEY
+
+export function findMatchingVideo (artist, songName, duration) {
+  return new Promise((resolve, reject) => {
+    findBestMusicVideo(`${artist} - ${songName}`, {
+      duration,
+      apiKey
+    }, (err, video) => {
+      if (err) return reject(err)
+
+      console.log('video', video)
+      resolve(video)
+    })
+  })
+}
