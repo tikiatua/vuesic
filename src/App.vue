27ce0d030c1bc07e241f7c46fe9510f239ca2404
Ramon Saccilotto <tikiatua@gmail.com>
2019-01-10 20:55:00 +0100

    adapt app component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="$style.app">
+
+    <logo />
+
+    <div id="configuration">
+      
+    </div>
+
+    <div id="search">
+
+    </div>
+
+    <div id="controls">
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/logo/Logo.vue';
+import Search from './components/search/Search.vue';
 
 export default {
   name: 'app',
@@ -15,12 +28,16 @@ export default {
   }
 }
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+<style lang="stylus" module>
+
+.app {
+  font-family 'Inter UI', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing antialiased;
+  -moz-osx-font-smoothing grayscale;
+  text-align center;
+  color #2c3e50;
+  margin-top 60px;
+}
+
 </style>
