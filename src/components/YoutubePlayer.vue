9a79401ccde815dc59a8fb109536382b71336448
Lukas Lücke <lukas@luecke.me>
2019-01-10 20:11:56 +0100

    Add YoutubePlayer component

---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,13 @@
+<template>
+    <youtube :video-id="videoId"></youtube>
+</template>
+<script>
+export default {
+  name: 'YoutubePlayer',
+  data () {
+    return {
+      videoId: 'lG0Ys-2d4MA'
+    }
+  }
+}
+</script>
